9db8aded62b78936ba57ea6d2716075e7be60604
Choi_donghyeon <104900866+derekop2000@users.noreply.github.com>
2024-04-10 01:24:52 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int s;
+	scanf("%d", &s);
+	if (s >= 90)putchar('A');
+	else if (s >= 80)putchar('B');
+	else if (s >= 70)putchar('C');
+	else if (s >= 60)putchar('D');
+	else putchar('F');
+}
