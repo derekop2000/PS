48eb94396a117c5c458add51a2f422c8f3dde9c6
Choi_donghyeon <104900866+derekop2000@users.noreply.github.com>
2024-04-10 01:24:24 +0900

    [Silver III] Title: 파도반 수열, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int dp[101];
+    dp[1]=1;
+    dp[2]=1;
+    dp[3]=1;
+    dp[4]=2;
+    dp[5]=2;
+    for(int i=6;i<=100;i++)
+        dp[i]=dp[i-1]+dp[i-5];
+
+
+    int t,n;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        cout << dp[n]<<'\n';
+    }
+}
