94f03f141571a9a69c8208482e51647625a69233
Choi_donghyeon <104900866+derekop2000@users.noreply.github.com>
2024-04-10 01:33:32 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int dp[1001];
+int main()
+{
+    int n;
+    cin >> n;
+    dp[0]=1;
+    dp[1]=1;
+
+    for(int i=2;i<=n;i++)
+    {
+        dp[i]=(dp[i-1]+dp[i-2]*2)%10007;
+    }
+    cout << dp[n];
+}
