9372bfb7b0ab62bcc2f5bb1cdb1c6e64330caa35
Choi_donghyeon <104900866+derekop2000@users.noreply.github.com>
2024-04-14 06:11:03 +0900

    [Gold III] Title: 개구리 점프, Time: 4 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.cc
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<tuple>
+#include<memory.h>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<map>
+using namespace std;
+
+vector<int> parent;
+int find_parent(int a)
+{
+	if (parent[a] == a)return a;
+	return parent[a] = find_parent(parent[a]);
+}
+void my_union(int a, int b)
+{
+	parent[find_parent(b)] = find_parent(a);
+}
+bool my_is_union(int a, int b)
+{
+	return find_parent(a) == find_parent(b);
+}
+int main()
+{
+	int n, q;
+	cin >> n >> q;
+	for (int i = 0; i < n; i++)
+		parent.push_back(i);
+	vector<tuple<int,int,int>> v;
+ 	for (int i = 0; i < n; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		v.push_back({ a ,b ,i });
+	}
+	sort(v.begin(), v.end());
+	int last = get<1>(v[0]);
+	for (int i = 1; i < n; i++)
+	{
+		if (last >= get<0>(v[i]))
+		{
+			last = max(last, get<1>(v[i]) );
+			my_union(get<2>(v[i - 1]) , get<2>(v[i]) );
+		}
+		else last = get<1>(v[i]);
+	}
+	for (int i = 0; i < q; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		a--; b--;
+		if (my_is_union(a, b))
+			cout << "1\n";
+		else cout << "0\n";
+	}
+}
