9bd6339df01a4ccbd4dbb77dd46a52dd90935825
Choi_donghyeon <104900866+derekop2000@users.noreply.github.com>
2024-06-14 04:15:09 +0900

    [Gold V] Title: 단어 마방진, Time: 48 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/24891. 단어 마방진/단어 마방진.cc
+++ 백준/Gold/24891. 단어 마방진/단어 마방진.cc
@@ -1,60 +1,87 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<tuple>
+#include<memory.h>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<iomanip>
+#include<climits>
+#include <functional>
 
 using namespace std;
 
-int L, N;
-vector<string> words;
-bool found = false;
+int dir[][2] = { {-1,0}, {0,1}, {1,0} ,{0,-1} };
 
-void checkMagicSquare(vector<string>& square) {
-    for (int i = 0; i < L; ++i) {
-        for (int j = 0; j < i; ++j) {
-            if (square[i][j] != square[j][i]) {
-                return;
-            }
-        }
-    }
-    found = true;
-    for (const string& row : square) {
-        cout << row << "\n";
-    }
+vector<int> parent;
+int findParent(int a)
+{
+	if (parent[a] == a)
+		return a;
+	return parent[a] = findParent(parent[a]);
+}
+void Union(int a, int b)
+{
+	parent[findParent(b)] = findParent(a);
+}
+bool IsUnion(int a, int b)
+{
+	return findParent(a) == findParent(b);
 }
 
-void permute(int depth, vector<string>& chosen, vector<bool>& used) {
-    if (found) return;
-    if (depth == L) {
-        checkMagicSquare(chosen);
-        return;
-    }
-    for (int i = 0; i < N; ++i) {
-        if (!used[i]) {
-            used[i] = true;
-            chosen[depth] = words[i];
-            permute(depth + 1, chosen, used);
-            if (found) return;
-            used[i] = false;
-        }
-    }
+int l, n;
+vector<string> s;
+vector<string> ss;
+bool ans = false;
+vector<bool> visit;
+
+void f2()
+{
+	for(int i=0;i<l;i++)
+		for (int j = 0; j < i; j++)
+		{
+			if (s[i][j] != s[j][i])
+				return;
+		}
+	ans = true;
+	for (int i = 0; i < l; i++)
+		cout << s[i] << "\n";
+}
+void f(int floor)
+{
+	if (floor == l)
+	{
+		f2();
+		return;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (visit[i])continue;
+		visit[i] = true;
+		s.push_back(ss[i]);
+		f(floor + 1);
+		if (ans)return;
+		visit[i] = false;
+		s.pop_back();
+	}
+}
+int main()
+{
+	cin >> l >> n;
+	visit.resize(n);
+	for (int i = 0; i < n; i++)
+	{
+		string s;
+		cin >> s;
+		::ss.push_back(s);
+	}
+	sort(ss.begin(), ss.end());
+	f(0);
+	if (ans==false)
+		cout << "NONE";
 }
 
-int main() {
-    cin >> L >> N;
-    words.resize(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> words[i];
-    }
-    sort(words.begin(), words.end());
-
-    vector<string> chosen(L);
-    vector<bool> used(N, false);
-    permute(0, chosen, used);
-
-    if (!found) {
-        cout << "NONE";
-    }
-
-    return 0;
-}
