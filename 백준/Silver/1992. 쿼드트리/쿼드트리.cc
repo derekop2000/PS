9b9279e22e79682a684bd222743a7c3dda504a98
Choi_donghyeon <104900866+derekop2000@users.noreply.github.com>
2024-08-19 18:43:45 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<tuple>
+#include<memory.h>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<iomanip>
+#include<climits>
+#include <functional>
+
+using namespace std;
+
+int dir[][2] = { {-1,0}, {0,1}, {1,0} ,{0,-1},{-1,1},{1,1},{1,-1},{-1,-1} };
+
+vector<int> parent;
+int findParent(int a)
+{
+	if (parent[a] == a)
+		return a;
+	return parent[a] = findParent(parent[a]);
+}
+void Union(int a, int b)
+{
+	parent[findParent(b)] = findParent(a);
+}
+bool IsUnion(int a, int b)
+{
+	return findParent(a) == findParent(b);
+}
+
+vector<vector<int>> arr;
+string ans = "";
+void f(int r, int c, int n)
+{
+	int temp = arr[r][c];
+	bool pass = true;
+	for (int i = r; i < r + n; i++)
+	{
+		for (int j = c; j < c + n; j++)
+		{
+			if (arr[i][j] != temp)
+			{
+				pass = false;
+			}
+		}
+	}
+	if (pass)
+	{
+		ans.push_back(temp+'0');
+	}
+	else
+	{
+		ans.push_back('(');
+		int range = n / 2;
+		f(r, c, range);
+		f(r, c+range, range);
+		f(r+ range, c, range);
+		f(r+ range, c+ range, range);
+		ans.push_back(')');
+	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	arr.resize(n, vector<int>(n));
+	for(int i=0;i<n;i++)
+		for (int j = 0; j < n; j++)
+		{
+			char c;
+			cin >> c;
+			if (c == '0')
+				arr[i][j] = 0;
+			else arr[i][j] = 1;
+		}
+	f(0, 0, n);
+	cout << ans;
+}
+
