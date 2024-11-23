72573ddb0ebd7af96b924110564934b751308e74
Choi_donghyeon <104900866+derekop2000@users.noreply.github.com>
2024-11-24 07:24:44 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 120 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -41,71 +41,76 @@ struct Node {
     Node* parent = nullptr;
 };
 
-int main() {
-    vector<int> v;
-    int temp;
-    while (cin >> temp) {
-        v.push_back(temp);
-    }
+int main()
+{
+	vector<int> v;
+	int temp;
+	while (cin >> temp)
+	{
+		v.push_back(temp);
+	}
+	
+	Node* root = new Node{};
+	Node* p = root;
+	root->value = v[0];
+	int top = v[0];
+	for (int i = 1; i < v.size(); i++) {
+		int now = v[i];
+		Node* newNode = new Node{};
+		newNode->value = now;
 
-    Node* root = new Node{};
-    root->value = v[0];
-    Node* p = root;
-
-    for (int i = 1; i < v.size(); i++) {
-        int now = v[i];
-        Node* newNode = new Node{};
-        newNode->value = now;
-
-        while (true) {
-            if (now < p->value)
-            { 
-                if (p->left == nullptr)
-                {
-                    p->left = newNode;
-                    newNode->parent = p;
-                    p = root;
-                    break;
-                }
-                p = p->left;
-            }
-            else
-            { 
-                if (p->right == nullptr)
-                {
-                    p->right = newNode;
-                    newNode->parent = p;
-                    p = root; 
-                    break;
-                }
-                p = p->right;
-            }
-        }
-    }
-
-    stack<Node*> s;
-    Node* curr = root;
-    Node* lastVisited = nullptr;
-
-    while (!s.empty() || curr != nullptr)
-    {
-        if (curr != nullptr)
-        {
-            s.push(curr);
-            curr = curr->left; 
-        }
-        else {
-            Node* peekNode = s.top();
-            
-            if (peekNode->right != nullptr && lastVisited != peekNode->right) {
-                curr = peekNode->right;
-            }
-            else {
-                cout << peekNode->value << "\n";
-                lastVisited = peekNode;
-                s.pop();
-            }
-        }
-    }
-
+		while (true) {
+			if (now < p->value)
+			{
+				if (p->left == nullptr)
+				{
+					p->left = newNode;
+					newNode->parent = p;
+					p = root;
+					break;
+				}
+				p = p->left;
+			}
+			else
+			{
+				if (p->right == nullptr)
+				{
+					p->right = newNode;
+					newNode->parent = p;
+					p = root;
+					break;
+				}
+				p = p->right;
+			}
+		}
+	}
+	p = root;
+	while (true)
+	{
+		if (p->left == nullptr)
+		{
+			if (p->right == nullptr)
+			{
+				cout << p->value << "\n";
+				if (p == root)
+					break;
+				p = p->parent;
+				if (p->left)
+				{
+					free(p->left);
+					p->left = nullptr;
+					continue;
+				}
+				if (p->right)
+				{
+					free(p->right);
+					p->right = nullptr;
+				}
+			}
+			else
+				p = p->right;
+		}
+		else
+			p = p->left;
+	}
 }
