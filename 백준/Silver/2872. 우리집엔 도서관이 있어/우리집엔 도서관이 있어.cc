e0e96bfe61b3382565102828309b3f538ace9784
Choi_donghyeon <104900866+derekop2000@users.noreply.github.com>
2025-02-23 21:47:48 +0900

    [Silver I] Title: 우리집엔 도서관이 있어, Time: 112 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.cc
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<tuple>
+#include<memory.h>
+#include<string>
+#include<sstream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<iomanip>
+#include<climits>
+#include <functional>
+#include <unordered_set>
+#include<stack>
+using namespace std;
+
+int dir[][2] = { {-1,0}, {0,1}, {1,0} ,{0,-1},{-1,1},{1,1},{1,-1},{-1,-1} };
+
+vector<int> parent;
+int findParent(int a)
+{
+	if (parent[a] == a)
+		return a;
+	return parent[a] = findParent(parent[a]);
+}
+void Union(int a, int b)
+{
+	parent[findParent(b)] = findParent(a);
+}
+bool IsUnion(int a, int b)
+{
+	return findParent(a) == findParent(b);
+}
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> arr(n+1);
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+	int num = n;
+	for (int i = n; i >= 1; i--)
+	{
+		if (arr[i] == num)
+		{
+			num--;
+		}
+	}
+	cout << num;
+}
